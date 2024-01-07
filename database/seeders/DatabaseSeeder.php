8648de1097ba58042b839c6b462d4b28f507f2b9
Oussama Madjmaa <oussama@madjmaa.com>
2024-01-07 08:05:07 +0400

    updates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,7 +26,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminUserSeeder::class,
             PlansSeeder::class,
-            TestDataSeeder::class
+            // TestDataSeeder::class
         ]);
     }
 }
