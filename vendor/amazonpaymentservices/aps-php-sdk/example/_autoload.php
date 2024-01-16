9a3b4084f528b96e5c273018144bf4db148fb254
Oussama Madjmaa <oussama@madjmaa.com>
2024-01-16 22:44:35 +0400

    remove vendor

--- vendor/amazonpaymentservices/aps-php-sdk/example/_autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-spl_autoload_register(function ($name) {
-    require_once str_replace("AmazonPaymentServicesSdk/", '../src/',
-            str_replace('\\', '/', $name)) . '.php';
-});
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
--- vendor/amazonpaymentservices/aps-php-sdk/example/_sample_app_config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'base_url'      => 'http://aps.localhost:8080/',
-];
