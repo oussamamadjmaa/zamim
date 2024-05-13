057395105c1c752b22b58471c891b72a8dcda0d0
Oussama Madjmaa <oussama@madjmaa.com>
2024-05-13 20:36:21 +0100

    updates

--- database/migrations/2023_12_26_153330_create_radio_students_table.php
+++ database/migrations/2023_12_26_153330_create_radio_students_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('radio_students', function (Blueprint $table) {
             $table->foreignId('radio_id')->constrained()->cascadeOnDelete();
             $table->foreignId('student_id')->references('id')->on('users')->cascadeOnDelete();
-            $table->foreignId('article_id')->constrained('articles')->nullOnDelete();
+            $table->foreignId('article_id')->nullable()->constrained('articles')->nullOnDelete();
             $table->unsignedTinyInteger('rating')->nullable();
             $table->primary(['radio_id', 'student_id']);
         });
